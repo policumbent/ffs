c639602e51d0994c213f80bcf1fb4bfe05b762f8
fraimparato <fra.imparato20@gmail.com>
2024-04-24 21:28:55 +0200

    add: test for systemd, journalctl functionalities in video module

--- modules/video/src/main.py
+++ modules/video/src/main.py
@@ -1,4 +1,15 @@
+import os, sys
+from time import sleep
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'lib')))
+from log import log
+
 def main():
+    while True:
+        log.err("Error")
+        log.warn("Warn")
+        log.info("Info")
+        sleep(1)
 
 if __name__ == 'main':
     main()
