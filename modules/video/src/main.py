ab7dbb9965f112c3ec0ee10e5332ed377cbca6f2
fraimparato <fra.imparato20@gmail.com>
2024-04-24 21:32:26 +0200

    fix: typo in sys.path.append in video module

--- modules/video/src/main.py
+++ modules/video/src/main.py
@@ -1,7 +1,7 @@
 import os, sys
 from time import sleep
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'lib')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'libs')))
 from log import log
 
 def main():
