6d0569dbd1b8539a264384a2936755bc99849c7c
fraimparato <fra.imparato20@gmail.com>
2024-04-28 15:51:14 +0200

    feat: pipe mode, test mode out of main, overlay_obj and overlay elements now global

--- modules/video/src/main.py
+++ modules/video/src/main.py
@@ -14,11 +14,103 @@ import json
 # append into path the libs folder, so that Python will find them
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'libs')))
 from log import log
+from pipe import Pipe
 
 
+TEST_MODE = 1
+
+
+# useful paths as strings
 home_path = os.getenv("HOME")
 ffs_path = f"{home_path}/ffs"
 config_path = f"{ffs_path}/config"
+
+FIFO_TO_VIDEO = "fifo_to_video"
+
+
+# overlay elements declaration
+speed = OverlayElement("ant/speed", unit=" kph", color=colors["blue"])
+distance = OverlayElement("ant/distance", unit=" m", color=colors["white"])
+power = OverlayElement("ant/power", unit=" W", color=colors["blue"])
+heartrate = OverlayElement("ant/heartrate", unit=" bpm", color=colors["red"])
+cadence = OverlayElement("ant/cadence", unit=" rpm", color=colors["green"])
+gear = OverlayElement("gb/gear", color=colors["white"])
+
+test_mode = OverlayElement("test", val="TEST")
+
+# overlay element positioning
+top_left_overlay = [speed, heartrate]
+top_right_overlay = [power, cadence]
+bottom_middle_overlay = [gear]
+bottom_right_overlay = [distance]
+
+if TEST_MODE:
+    top_middle_overlay = [test_mode]
+
+# overlay declaration
+overlay_obj = Overlay(
+    video_conf["screen"]["width"],
+    video_conf["screen"]["height"],
+    rotation=video_conf["overlay_rotation"],
+    top_left=top_left_overlay,
+    top_middle=top_middle_overlay,
+    top_right=top_right_overlay,
+    bottom_middle=bottom_middle_overlay,
+    bottom_right=bottom_right_overlay
+)
+
+
+def test_mode(picam):
+    while True:
+        for i in range(11):
+            speed.set_value(i)
+            distance.set_value(i)
+            power.set_value(i)
+            heartrate.set_value(i)
+            cadence.set_value(i)
+            gear.set_value(i)
+            overlay = overlay_obj.update_overlay()
+            picam.set_overlay(overlay)
+            #log.info("VIDEO - Overlay in progress")
+            sleep(1)
+
+
+def update_values(type, val):
+    if type == "ant/speed":
+        speed.set_value(val)
+        overlay_obj.update_overlay()
+        
+    elif type == "ant/distance":
+        distance.set_value(val)
+        overlay_obj.update_overlay()
+        
+    elif type == "ant/power":
+        power.set_value(val)
+        overlay_obj.update_overlay()
+        
+    elif type == "ant/heartrate":
+        heartrate.set_value(val)
+        overlay_obj.update_overlay()
+        
+    elif type == "ant/cadence":
+        cadence.set_value(val)
+        overlay_obj.update_overlay()
+        
+    elif type == "gb/gear":
+        gear.set_value(val)
+        overlay_obj.update_overlay()
+
+
+def fifo_mode(pipe, picam):
+    while True:
+        try:
+            if pipe.read():
+                for rd in pipe.get_data().rstrip().split("-"):
+                    if rd != "":
+                        sensor, value = rd.split(":")
+                        update_values(sensor, value)
+        except Exception as e:
+            log.err(f"FIFO: {e}")
 
 
 def json_to_dict(path: str):
@@ -76,44 +168,12 @@ def main():
     )
     picam.start()
 
-    speed = OverlayElement("ant/speed", unit=" kph", color=colors["blue"])
-    distance = OverlayElement("ant/distance", unit=" m", color=colors["white"])
-    power = OverlayElement("ant/power", unit=" W", color=colors["blue"])
-    heartrate = OverlayElement("ant/heartrate", unit=" bpm", color=colors["red"])
-    cadence = OverlayElement("ant/cadence", unit=" rpm", color=colors["green"])
-    gear = OverlayElement("gb/gear", color=colors["white"])
-
-    top_left_overlay = [speed, heartrate]
-    top_right_overlay = [power, cadence]
-    bottom_middle_overlay = [gear]
-    bottom_right_overlay = [distance]
-
-    overlay_obj = Overlay(
-        video_conf["screen"]["width"],
-        video_conf["screen"]["height"],
-        rotation=video_conf["overlay_rotation"],
-        top_left=top_left_overlay,
-        top_right=top_right_overlay,
-        bottom_middle=bottom_middle_overlay,
-        bottom_right=bottom_right_overlay
-    )
-
-    #pause()
-
-    # TEST MODE -> to use it, decomment the following code and comment the
-    # pause() above
-    while True:
-        for i in range(11):
-            speed.set_value(i)
-            distance.set_value(i)
-            power.set_value(i)
-            heartrate.set_value(i)
-            cadence.set_value(i)
-            gear.set_value(i)
-            overlay = overlay_obj.update_overlay()
-            picam.set_overlay(overlay)
-            #log.info("VIDEO - Overlay in progress")
-            sleep(1)
+    if TEST_MODE:
+        test_mode(picam)
+    else:
+        # hybrid solution with pipe still in bob
+        pipe = Pipe(f"{home_path}/bob/{FIFO_TO_VIDEO}", "r")
+        fifo_mode(pipe, picam)
 
 
 if __name__ == '__main__':
